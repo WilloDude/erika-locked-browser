6dc9edbfa9817983df511c3f8113bbc87ec2828b
William M <billymillena@gmail.com>
2023-03-28 21:58:17 +1100

    redcliff.gov UPDATE
    Still a WIP, added a enlist option, working on news rn.
    @ChairmanMaoZedong is now a collaber

---
+++ websites/REDCLIFF[gov].py
@@ -0,0 +1,7 @@
+print("Welcome to the great Federation of Redcliff!")
+print("SERVE THE MOTHERLAND")
+print("ENLIST NOW!")
+nstory1= input("Enlist? (Y/N) ")
+if nstory1 == input("Y") or input("y"):
+    print("Zdrasvuyte!")
+    cit1= input("Are you a Citizen of the Peoples Federation of Redcliff? ")
